e7dd474ad445f7fe1498afbc945072d1ff6f67da
Svitlana Filippova <s.rubinets@gmail.com>
2024-07-09 08:21:10 +0300

    Добавила тулбар в Медиатеку

--- app/src/main/java/com/practicum/playlistmaker/LibraryActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/LibraryActivity.kt
@@ -3,10 +3,21 @@ package com.practicum.playlistmaker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.databinding.ActivityLibraryBinding
+import com.practicum.playlistmaker.databinding.ActivitySettingsBinding
 
 class LibraryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLibraryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_library)
+        binding = ActivityLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.libraryToolbar.setNavigationOnClickListener() {
+            finish()
+        }
+
+
     }
 }
