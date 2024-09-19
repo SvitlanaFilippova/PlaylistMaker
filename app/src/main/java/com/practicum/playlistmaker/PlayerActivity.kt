46f8cbd900890aab5e8292d30fd854c8854e3d7d
Svitlana Filippova <s.rubinets@gmail.com>
2024-09-19 13:46:28 +0300

    Реализовано отображение текущего времени воспроизведения отрывка трека,

--- app/src/main/java/com/practicum/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/PlayerActivity.kt
@@ -6,14 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.gson.Gson
 import com.practicum.playlistmaker.databinding.ActivityPlayerBinding
-import java.time.LocalDateTime
 import java.util.Locale
 
 
@@ -22,8 +21,9 @@ class PlayerActivity() : AppCompatActivity() {
     private var mediaPlayer = MediaPlayer()
     private lateinit var buttonPlay: ImageButton
     private lateinit var url: String
+    private lateinit var tvTrackProgress: TextView
     private var mainThreadHandler: Handler? = null
-    private var trackTimeProgress = formatTime(0L)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,16 +35,15 @@ class PlayerActivity() : AppCompatActivity() {
         binding.apply {
 
             ibArrowBack.setOnClickListener() { finish() }
+
             buttonPlay = ibPlay
             buttonPlay.isEnabled = false
             url = track.previewUrl
             preparePlayer()
-
-            tvTrackProgress.text = trackTimeProgress
+            tvTrackProgress.text = formatTime(0)
             tvTrackName.text = track.trackName
             tvArtistName.text = track.artistName
-            tvDurationTrack.text =
-                SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTimeMillis)
+            tvDurationTrack.text = formatTime(track.trackTimeMillis)
             tvYearTrack.text = track.releaseDate.slice(0..3)
 
             tvGenreTrack.text = track.primaryGenreName
@@ -69,6 +68,7 @@ class PlayerActivity() : AppCompatActivity() {
 
             buttonPlay.setOnClickListener {
                 playbackControl()
+
             }
         }
 
@@ -84,6 +84,8 @@ class PlayerActivity() : AppCompatActivity() {
         }
         mediaPlayer.setOnCompletionListener {
             buttonPlay.setImageResource(R.drawable.ic_play)
+            stopRefreshingProgress()
+            binding.tvTrackProgress.text = formatTime(0)
             playerState = STATE_PREPARED
         }
     }
@@ -92,12 +94,15 @@ class PlayerActivity() : AppCompatActivity() {
         mediaPlayer.start()
         buttonPlay.setImageResource(R.drawable.ic_pause)
         playerState = STATE_PLAYING
+        startRefreshingProgress()
+
     }
 
     private fun pausePlayer() {
         mediaPlayer.pause()
         buttonPlay.setImageResource(R.drawable.ic_play)
         playerState = STATE_PAUSED
+        stopRefreshingProgress()
     }
 
     private fun playbackControl() {
@@ -112,7 +117,28 @@ class PlayerActivity() : AppCompatActivity() {
         }
     }
 
-    private fun formatTime(time: Long): String {
+    val refreshProgressRunnable = object : Runnable {
+        override fun run() {
+            binding.tvTrackProgress.text = formatTime(mediaPlayer.currentPosition)
+
+            mainThreadHandler?.postDelayed(
+                this,
+                PROGRESS_REFRESH_DELAY_MILLIS,
+            )
+        }
+    }
+
+    fun startRefreshingProgress() {
+        mainThreadHandler?.postDelayed(
+            refreshProgressRunnable, PROGRESS_REFRESH_DELAY_MILLIS
+        )
+    }
+
+    fun stopRefreshingProgress() {
+        mainThreadHandler?.removeCallbacks(refreshProgressRunnable)
+    }
+
+    private fun formatTime(time: Int): String {
         return SimpleDateFormat("mm:ss", Locale.getDefault()).format(time)
     }
 
@@ -123,6 +149,7 @@ class PlayerActivity() : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+        stopRefreshingProgress()
         mediaPlayer.release()
     }
 
@@ -131,6 +158,7 @@ class PlayerActivity() : AppCompatActivity() {
         private const val STATE_PREPARED = 1
         private const val STATE_PLAYING = 2
         private const val STATE_PAUSED = 3
+        private const val PROGRESS_REFRESH_DELAY_MILLIS = 400L
     }
 
     private var playerState = STATE_DEFAULT
