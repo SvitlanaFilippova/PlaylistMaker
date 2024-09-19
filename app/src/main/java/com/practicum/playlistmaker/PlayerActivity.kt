85caa8b5ae5a3311a9e6c9fcd39fbf978a184395
Svitlana Filippova <s.rubinets@gmail.com>
2024-09-19 13:05:42 +0300

    Реализовано воспроизведение демо-отрывков треков в плеере

--- app/src/main/java/com/practicum/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/PlayerActivity.kt
@@ -1,8 +1,13 @@
 package com.practicum.playlistmaker
 
 import android.icu.text.SimpleDateFormat
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.ImageButton
 import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -14,7 +19,11 @@ import java.util.Locale
 
 class PlayerActivity() : AppCompatActivity() {
     private lateinit var binding: ActivityPlayerBinding
-
+    private var mediaPlayer = MediaPlayer()
+    private lateinit var buttonPlay: ImageButton
+    private lateinit var url: String
+    private var mainThreadHandler: Handler? = null
+    private var trackTimeProgress = formatTime(0L)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +31,16 @@ class PlayerActivity() : AppCompatActivity() {
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mainThreadHandler = Handler(Looper.getMainLooper())
         binding.apply {
 
             ibArrowBack.setOnClickListener() { finish() }
+            buttonPlay = ibPlay
+            buttonPlay.isEnabled = false
+            url = track.previewUrl
+            preparePlayer()
+
+            tvTrackProgress.text = trackTimeProgress
             tvTrackName.text = track.trackName
             tvArtistName.text = track.artistName
             tvDurationTrack.text =
@@ -50,6 +66,72 @@ class PlayerActivity() : AppCompatActivity() {
                 tvCollectionTrack.isVisible = false
                 tvCollectionTitle.isVisible = false
             }
+
+            buttonPlay.setOnClickListener {
+                playbackControl()
+            }
+        }
+
+
+    }
+
+    fun preparePlayer() {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            buttonPlay.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            buttonPlay.setImageResource(R.drawable.ic_play)
+            playerState = STATE_PREPARED
         }
     }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        buttonPlay.setImageResource(R.drawable.ic_pause)
+        playerState = STATE_PLAYING
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        buttonPlay.setImageResource(R.drawable.ic_play)
+        playerState = STATE_PAUSED
+    }
+
+    private fun playbackControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private fun formatTime(time: Long): String {
+        return SimpleDateFormat("mm:ss", Locale.getDefault()).format(time)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+    }
+
+    private var playerState = STATE_DEFAULT
 }
