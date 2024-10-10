b3e7f7d7762d364210faa7627cfc862451ff79e4
Svitlana Filippova <s.rubinets@gmail.com>
2024-10-10 13:44:24 +0300

    Рефакторинг PlayerActivity под паттерн MVVM

--- app/src/main/java/com/practicum/playlistmaker/ui/player/view_model/PlayerState.kt
+++ app/src/main/java/com/practicum/playlistmaker/ui/player/view_model/PlayerState.kt
@@ -1,8 +1,8 @@
 package com.practicum.playlistmaker.ui.player.view_model
 
 sealed class PlayerState {
-    object Default : PlayerState()
-    object Prepared : PlayerState()
-    object Playing : PlayerState()
-    object Paused : PlayerState()
+    data object Default : PlayerState()
+    data object Prepared : PlayerState()
+    data object Playing : PlayerState()
+    data object Paused : PlayerState()
 }
